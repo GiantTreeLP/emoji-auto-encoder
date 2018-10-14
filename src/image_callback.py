d260eb60f00401fecbab1e8290a8984f567074aa
Marvin <marv.1997@live.de>
2018-10-14 14:29:30 +0200

    Only save every n periods

--- src/image_callback.py
+++ src/image_callback.py
@@ -7,24 +7,29 @@ from tensorflow import keras
 
 class TensorBoardImage(keras.callbacks.TensorBoard):
 
-    def __init__(self, log_dir, tag, images):
-        super().__init__(log_dir=log_dir)
+    def __init__(self, log_dir, tag, images, period=1):
+        super(TensorBoardImage, self).__init__(log_dir=log_dir)
         self.tag = tag
         self.images = images
+        self.period = period
         cfg = tf.ConfigProto()
         self.sess = tf.Session(config=cfg)
+        self.last_save = 0
 
     def on_epoch_end(self, epoch, logs=None):
         super(TensorBoardImage, self).on_epoch_end(epoch, logs)
-        if logs is None:
-            logs = {}
-        prediction = self.model.predict([[self.images[0]]])[0]
-        prediction = prediction * 255
-        prediction = prediction.astype('uint8')
-        img_bytes = io.BytesIO()
-        image = Image.fromarray(prediction)
-        image.save(img_bytes, format="png")
-        image = tf.Summary.Image(height=image.height, width=image.width, encoded_image_string=img_bytes.getvalue())
-        summary = tf.Summary(value=[tf.Summary.Value(tag=self.tag, image=image)])
 
-        self.writer.add_summary(summary, epoch)
+        self.last_save += 1
+        if self.last_save >= self.period:
+            self.last_save = 0
+
+            prediction = self.model.predict([[self.images[0]]])[0]
+            prediction = prediction * 255
+            prediction = prediction.astype('uint8')
+            img_bytes = io.BytesIO()
+            image = Image.fromarray(prediction)
+            image.save(img_bytes, format="png")
+            image = tf.Summary.Image(height=image.height, width=image.width, encoded_image_string=img_bytes.getvalue())
+            summary = tf.Summary(value=[tf.Summary.Value(tag=self.tag, image=image)])
+
+            self.writer.add_summary(summary, epoch)
