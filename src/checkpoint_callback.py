5013a609b559b3ca0fed89bd64ff53f946a90fd3
Marvin <marv.1997@live.de>
2018-10-14 15:25:36 +0200

    Only log when wanted

--- src/checkpoint_callback.py
+++ src/checkpoint_callback.py
@@ -18,9 +18,12 @@ class CheckpointCallback(ModelCheckpoint):
                                                  mode,
                                                  period)
         self.logger = ProgbarLogger()
+        self.logger.verbose = True
 
     def on_epoch_begin(self, epoch, logs=None):
-        self.logger.on_epoch_begin(epoch, logs)
+        super(CheckpointCallback, self).on_epoch_begin(epoch, logs)
+        if self.epochs_since_last_save == 0:
+            self.logger.on_epoch_begin(epoch, logs)
 
     def on_epoch_end(self, epoch, logs=None):
         super().on_epoch_end(epoch, logs)
@@ -29,13 +32,30 @@ class CheckpointCallback(ModelCheckpoint):
             self.logger.on_epoch_end(epoch, logs)
 
     def on_train_begin(self, logs=None):
-        self.logger.on_train_begin(logs)
+        super(CheckpointCallback, self).on_train_begin(logs)
+        if self.epochs_since_last_save == 0:
+            self.logger.on_train_begin(logs)
 
     def on_train_end(self, logs=None):
-        self.logger.on_epoch_end(logs)
+        super(CheckpointCallback, self).on_train_end(logs)
+        if self.epochs_since_last_save == 0:
+            self.logger.on_epoch_end(logs)
 
     def on_batch_begin(self, batch, logs=None):
-        self.logger.on_batch_begin(batch, logs)
+        super(CheckpointCallback, self).on_batch_begin(batch)
+        if self.epochs_since_last_save == 0:
+            self.logger.on_batch_begin(batch, logs)
 
     def on_batch_end(self, batch, logs=None):
-        self.logger.on_batch_end(batch, logs)
+        super(CheckpointCallback, self).on_batch_end(batch)
+        if self.epochs_since_last_save == 0:
+            self.logger.on_batch_end(batch, logs)
+
+    def set_model(self, model):
+        super(CheckpointCallback, self).set_model(model)
+        self.logger.set_model(model)
+
+    def set_params(self, params):
+        super(CheckpointCallback, self).set_params(params)
+        params['verbose'] = True
+        self.logger.set_params(params)
