fdb789c76308defabab378c653adb73ce7f91ed6
Marvin <marv.1997@live.de>
2018-10-15 19:42:56 +0200

    Use VarianceScaling as the kernel initializer as glorot_uniform is not understood on tensorflowjs

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -18,13 +18,14 @@ from image_callback import TensorBoardImage
 
 def encoder_128(vector_len: int) -> Model:
     input_img = Input(shape=(128, 128, 1), name="input_128x128")  # 128x128
-    x = Conv2D(16, (5, 5), activation=relu, padding='same', name="Convolution1")(input_img)
+    x = Conv2D(16, (5, 5), activation=relu, kernel_initializer='VarianceScaling', padding='same', name="Convolution1")(
+        input_img)
     x = MaxPooling2D((2, 2), padding='same', name="shrink_64x64")(x)  # 64x64
-    x = Conv2D(8, (3, 3), activation=relu, padding='same', name="Convolution2")(x)
+    x = Conv2D(8, (3, 3), activation=relu, kernel_initializer='VarianceScaling', padding='same', name="Convolution2")(x)
     x = MaxPooling2D((2, 2), padding='same', name="shrink_32x32")(x)  # 32x32
-    x = Conv2D(8, (3, 3), activation=relu, padding='same', name="Convolution3")(x)
+    x = Conv2D(8, (3, 3), activation=relu, kernel_initializer='VarianceScaling', padding='same', name="Convolution3")(x)
     x = MaxPooling2D((4, 4), padding='same', name="shrink_8x8")(x)
-    x = Conv2D(4, (3, 3), activation=relu, padding='same')(x)
+    x = Conv2D(4, (3, 3), activation=relu, kernel_initializer='VarianceScaling', padding='same')(x)
     x = MaxPooling2D((2, 2), padding='same', name="shrink_4x4")(x)
     x = Flatten(name="matrix-to-vector")(x)
     x = Dense(64, activation=relu, name="link_flat_to_64x1")(x)
@@ -37,15 +38,16 @@ def decoder_128(vector_len: int) -> Model:
     x = Dense(64, activation=tanh, name="activate_input")(input_decoder)
     x = Dense(64, activation=relu, name="link_reshape_64x1")(x)
     x = Reshape((8, 8, 1), name="reshape_8x8")(x)
-    x = Conv2D(8, (3, 3), activation=relu, padding='same')(x)
+    x = Conv2D(8, (3, 3), activation=relu, kernel_initializer='VarianceScaling', padding='same')(x)
     x = UpSampling2D((2, 2), name="grow_16x16")(x)
-    x = Conv2D(8, (3, 3), activation=relu, padding='same')(x)
+    x = Conv2D(8, (3, 3), activation=relu, kernel_initializer='VarianceScaling', padding='same')(x)
     x = UpSampling2D((2, 2), name="grow_32x32")(x)
-    x = Conv2D(8, (3, 3), activation=relu, padding='same')(x)
+    x = Conv2D(8, (3, 3), activation=relu, kernel_initializer='VarianceScaling', padding='same')(x)
     x = UpSampling2D((2, 2), name="grow_64x64")(x)
-    x = Conv2D(16, (3, 3), activation=relu, padding='same')(x)
+    x = Conv2D(16, (3, 3), activation=relu, kernel_initializer='VarianceScaling', padding='same')(x)
     x = UpSampling2D((2, 2), name="grow_128x128")(x)
-    decoded = Conv2D(1, (5, 5), activation=tanh, padding='same', name="output_128x128")(x)
+    decoded = Conv2D(1, (5, 5), activation=tanh, kernel_initializer='VarianceScaling', padding='same',
+                     name="output_128x128")(x)
 
     return Model(input_decoder, decoded, name="Decoder")
 
