97e595458a494612fedc3035f8bfe607a56ebdc9
Marvin <marv.1997@live.de>
2018-10-15 20:11:50 +0200

    Extract logs dir constant

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -15,17 +15,18 @@ from tensorflow.python.keras.optimizers import Adadelta
 from checkpoint_callback import CheckpointCallback
 from image_callback import TensorBoardImage
 
+LOGS_DIR = "../logs/"
+
 
 def encoder_128(vector_len: int) -> Model:
     input_img = Input(shape=(128, 128, 1), name="input_128x128")  # 128x128
-    x = Conv2D(16, (5, 5), activation=relu, kernel_initializer='VarianceScaling', padding='same', name="Convolution1")(
-        input_img)
+    x = Conv2D(16, (5, 5), activation=relu, padding='same', name="Convolution1")(input_img)
     x = MaxPooling2D((2, 2), padding='same', name="shrink_64x64")(x)  # 64x64
-    x = Conv2D(8, (3, 3), activation=relu, kernel_initializer='VarianceScaling', padding='same', name="Convolution2")(x)
+    x = Conv2D(8, (3, 3), activation=relu, padding='same', name="Convolution2")(x)
     x = MaxPooling2D((2, 2), padding='same', name="shrink_32x32")(x)  # 32x32
-    x = Conv2D(8, (3, 3), activation=relu, kernel_initializer='VarianceScaling', padding='same', name="Convolution3")(x)
+    x = Conv2D(8, (3, 3), activation=relu, padding='same', name="Convolution3")(x)
     x = MaxPooling2D((4, 4), padding='same', name="shrink_8x8")(x)
-    x = Conv2D(4, (3, 3), activation=relu, kernel_initializer='VarianceScaling', padding='same')(x)
+    x = Conv2D(4, (3, 3), activation=relu, padding='same')(x)
     x = MaxPooling2D((2, 2), padding='same', name="shrink_4x4")(x)
     x = Flatten(name="matrix-to-vector")(x)
     x = Dense(64, activation=relu, name="link_flat_to_64x1")(x)
@@ -38,16 +39,15 @@ def decoder_128(vector_len: int) -> Model:
     x = Dense(64, activation=tanh, name="activate_input")(input_decoder)
     x = Dense(64, activation=relu, name="link_reshape_64x1")(x)
     x = Reshape((8, 8, 1), name="reshape_8x8")(x)
-    x = Conv2D(8, (3, 3), activation=relu, kernel_initializer='VarianceScaling', padding='same')(x)
+    x = Conv2D(8, (3, 3), activation=relu, padding='same')(x)
     x = UpSampling2D((2, 2), name="grow_16x16")(x)
-    x = Conv2D(8, (3, 3), activation=relu, kernel_initializer='VarianceScaling', padding='same')(x)
+    x = Conv2D(8, (3, 3), activation=relu, padding='same')(x)
     x = UpSampling2D((2, 2), name="grow_32x32")(x)
-    x = Conv2D(8, (3, 3), activation=relu, kernel_initializer='VarianceScaling', padding='same')(x)
+    x = Conv2D(8, (3, 3), activation=relu, padding='same')(x)
     x = UpSampling2D((2, 2), name="grow_64x64")(x)
-    x = Conv2D(16, (3, 3), activation=relu, kernel_initializer='VarianceScaling', padding='same')(x)
+    x = Conv2D(16, (3, 3), activation=relu, padding='same')(x)
     x = UpSampling2D((2, 2), name="grow_128x128")(x)
-    decoded = Conv2D(1, (5, 5), activation=tanh, kernel_initializer='VarianceScaling', padding='same',
-                     name="output_128x128")(x)
+    decoded = Conv2D(1, (5, 5), activation=tanh, padding='same', name="output_128x128")(x)
 
     return Model(input_decoder, decoded, name="Decoder")
 
@@ -67,8 +67,8 @@ def train_model(model: Model, images):
     time_str = datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S")
 
     callbacks = [
-        TensorBoardImage(f'../logs/{time_str}', "Emojis", images, period=10),
-        CheckpointCallback(f'../logs/{time_str}', period=10),
+        TensorBoardImage(f'{LOGS_DIR}{time_str}', "Emojis", images, period=10),
+        CheckpointCallback(f'{LOGS_DIR}{time_str}', period=10),
     ]
     model.fit(images, images, epochs=100000, batch_size=len(images),
               # validation_data=(images, images),
@@ -79,13 +79,15 @@ def train_model(model: Model, images):
 
 def get_model():
     model, encoder, decoder = create_model(8)
-    dirs = [x for x in os.listdir("../logs/") if not path.isfile(f"../logs{x}")]
+    if path.exists(LOGS_DIR):
+        dirs = [x for x in os.listdir(LOGS_DIR) if not path.isfile(f"{LOGS_DIR}{x}")]
+        dirs.sort()
 
-    if path.exists(f"../logs/{dirs[-1]}/model.h5"):
-        try:
-            model.load_weights(f"../logs/{dirs[-1]}/model.h5")
-        except ImportError:
-            pass
+        if path.exists(f"{LOGS_DIR}{dirs[-1]}/model.h5"):
+            try:
+                model.load_weights(f"{LOGS_DIR}{dirs[-1]}/model.h5")
+            except ImportError:
+                pass
     return model, encoder, decoder
 
 
