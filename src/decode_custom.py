814b213d736d8d2be6afcb9aff63aa616b9ca9de
Marvin <marv.1997@live.de>
2018-10-16 19:11:55 +0200

    Run predictions on all images for comparison

--- src/decode_custom.py
+++ src/decode_custom.py
@@ -1,12 +1,28 @@
+import glob
+import os
+
 import imageio
 import numpy as np
 
 from autoencoder import get_model
 
 if __name__ == '__main__':
-    _, _, decoder = get_model()
-    input_array = [0 for x in range(64)]
+    images = []
+    names = []
+    for file in glob.glob("../emojis/twemoji/png_bw/*.png"):
+        images.append(imageio.imread(file))
+        names.append(os.path.splitext(os.path.basename(file))[0])
 
-    prediction = decoder.predict([[np.array(input_array)]])
-    print(prediction[0])
+    images = np.array(images)
+    images = np.reshape(images, (-1, 128, 128, 1))
+    images = images.astype('float32') / 255
+    model, _, _ = get_model()
+
+    prediction = model.predict(images)
+    prediction = (prediction * 255).astype('uint8')
+
+    os.makedirs("../emojis/twemoji/test", exist_ok=True)
+    for i in range(len(prediction)):
+        imageio.imwrite(f"../emojis/twemoji/test/{names[i]}.png", prediction[i], 'png')
+
     imageio.imwrite("../test.png", prediction[0])
