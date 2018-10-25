8817e5344ca259eef5eb14fd4c6f44a382b43c91
Marvin <marv.1997@live.de>
2018-10-25 22:57:09 +0200

    Refactor code and allow manual inputs

--- www/sketch.js
+++ www/sketch.js
@@ -1,10 +1,7 @@
 let sketch = function (s) {
 
-    s.sliderChanged = async function () {
-        for (let i = 0; i < s.parameters.length; i++) {
-            s.spans[i].value = s.parameters[i].value();
-        }
-        tf.tidy(() => {
+    async function renderEmoji() {
+        return tf.tidy(() => {
             s.model.outputLayers[0].predict(tf.tensor2d([s.parameters.map(p => p.value())])).data()
                 .then(arr => {
                     let b = tf.scalar(0);
@@ -12,17 +9,33 @@ let sketch = function (s) {
                     tf.toPixels(a, s.canvas.canvas);
                 });
         });
+    }
+
+    s.sliderChanged = async function () {
+        for (let i = 0; i < s.parameters.length; i++) {
+            s.inputs[i].value(s.parameters[i].value());
+        }
+        await renderEmoji();
+    };
+
+    s.newInput = async function (event) {
+        for (let i = 0; i < s.inputs.length; i++) {
+            s.parameters[i].value(s.inputs[i].value());
+        }
+        if (!isNaN(parseInt(event.target.value))) {
+            await renderEmoji();
+        }
     };
 
     s.setup = async function () {
         s.canvas = s.createCanvas(128, 128);
         s.parameters = [];
-        s.spans = [];
+        s.inputs = [];
         for (let i = 1; i < 9; i++) {
             let div = s.createDiv();
             s.createSpan(`Variable ${i}: `).parent(div);
             s.parameters.push(s.createSlider(-1, 1, 0, 0.00001).parent(div).input(s.sliderChanged).size(384));
-            s.spans.push(s.createSpan().parent(div));
+            s.inputs.push(s.createInput("0").parent(div).input(s.newInput));
         }
 
         s.model = await tf.loadModel("./model.json");
