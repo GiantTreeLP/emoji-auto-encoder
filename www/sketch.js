fe8f7f4fb26d81e53420f99bdae3abc6506362c3
Marvin <marv.1997@live.de>
2018-10-25 23:36:38 +0200

    Add permalink function

--- www/sketch.js
+++ www/sketch.js
@@ -1,6 +1,6 @@
 let sketch = function (s) {
 
-    async function renderEmoji() {
+    s.renderEmoji = function () {
         return tf.tidy(() => {
             s.model.outputLayers[0].predict(tf.tensor2d([s.parameters.map(p => p.value())])).data()
                 .then(arr => {
@@ -9,21 +9,32 @@ let sketch = function (s) {
                     tf.toPixels(a, s.canvas.canvas);
                 });
         });
-    }
+    };
 
     s.sliderChanged = async function () {
         for (let i = 0; i < s.parameters.length; i++) {
             s.inputs[i].value(s.parameters[i].value());
         }
-        await renderEmoji();
+        s.updatePermalink();
+        await s.renderEmoji();
+    };
+
+    s.updatePermalink = function () {
+        s.permalink.html("");
+        const url = new URL(document.location);
+        for (let i = 0; i < s.inputs.length; i++) {
+            url.searchParams.set(`v${i}`, s.parameters[i].value());
+        }
+        s.createA(url.toString(), url.toString()).parent(s.permalink);
     };
 
     s.newInput = async function (event) {
         for (let i = 0; i < s.inputs.length; i++) {
             s.parameters[i].value(s.inputs[i].value());
         }
-        if (!isNaN(parseInt(event.target.value))) {
-            await renderEmoji();
+        s.updatePermalink();
+        if (event && !isNaN(parseInt(event.target.value))) {
+            await s.renderEmoji();
         }
     };
 
@@ -31,15 +42,19 @@ let sketch = function (s) {
         s.canvas = s.createCanvas(128, 128);
         s.parameters = [];
         s.inputs = [];
+        const url = new URL(document.location);
         for (let i = 1; i < 9; i++) {
             let div = s.createDiv();
             s.createSpan(`Variable ${i}: `).parent(div);
-            s.parameters.push(s.createSlider(-1, 1, 0, 0.00001).parent(div).input(s.sliderChanged).size(384));
-            s.inputs.push(s.createInput("0").parent(div).input(s.newInput));
+            let value = parseFloat(url.searchParams.get(`v${i}`) || 0);
+            s.parameters.push(s.createSlider(-1, 1, value, 0.00001).parent(div).input(s.sliderChanged).size(384));
+            s.inputs.push(s.createInput(value).parent(div).input(s.newInput));
         }
+        s.permalink = s.createDiv();
 
         s.model = await tf.loadModel("./model.json");
-        await s.sliderChanged();
+        await s.newInput();
+        await s.renderEmoji();
     };
 };
 
