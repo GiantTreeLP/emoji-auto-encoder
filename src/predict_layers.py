a10de5f6359a96d1666629a069a102dc08187546
Marvin <marv.1997@live.de>
2019-01-18 15:34:06 +0100

    Fix K function call and image reshaping

--- src/predict_layers.py
+++ src/predict_layers.py
@@ -21,7 +21,7 @@ def load_images() -> Tuple[np.ndarray, list]:
         images.append(imageio.imread(file))
         names.append(os.path.splitext(os.path.basename(file))[0])
     images = np.array(images)
-    images = np.reshape(images, (-1, 32, 32, 1))
+    images = np.reshape(images, (-1, 128, 128, 1))
     images = images.astype('float32') / 255
     return images, names
 
@@ -36,21 +36,19 @@ def main():
         intermediate_output = None
         for i in range(1, len(encoder.layers)):
             os.makedirs(f"../test/encoder_{i}", exist_ok=True)
-            model = K.function([encoder.layers[0].input], [encoder.layers[i].output])
+            model = K.function([encoder.layers[0].input, K.learning_phase()], [encoder.layers[i].output])
             intermediate_output = model([[image]])[0]
             if len(intermediate_output.shape) == 4:
                 intermediate_output = np.swapaxes(intermediate_output, 0, 3)
                 intermediate_output = (intermediate_output * 255).astype('uint8')
                 for j in range(len(intermediate_output)):
                     imageio.imwrite(f"../test/encoder_{i}/{j}.png", intermediate_output[j], "png")
-            if len(intermediate_output.shape) == 2:
-                pass
 
         encoded = intermediate_output
 
         for i in range(1, len(decoder.layers)):
             os.makedirs(f"../test/decoder_{i}", exist_ok=True)
-            model = K.function([decoder.layers[0].input], [decoder.layers[i].output])
+            model = K.function([decoder.layers[0].input, K.learning_phase()], [decoder.layers[i].output])
             intermediate_output = model([encoded])[0]
             if len(intermediate_output.shape) == 4:
                 intermediate_output = np.swapaxes(intermediate_output, 0, 3)
