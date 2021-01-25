ea01e31a7a6e936f3ddb04b69e96dcc93617e8e1
Marvin <marv.1997@live.de>
2021-01-25 18:44:01 +0100

    Adapt to RGBA images (3 dimensions with 4 channels)
    Signed-off-by: Marvin <marv.1997@live.de>

--- src/image_callback.py
+++ src/image_callback.py
@@ -9,15 +9,14 @@ class TensorBoardImage(keras.callbacks.Callback):
         super().__init__()
         self.log_dir = log_dir
         self.tag = tag
-        self.sample_image = np.reshape(images[0:3], (-1, 128, 128, 1))
+        self.sample_image = np.reshape(images[0:3], (-1, 128, 128, 4))
         self.period = period
         self.last_save = 0
         self.writer = tf.summary.create_file_writer(self.log_dir)
 
     def on_train_begin(self, logs=None):
-        image = (self.sample_image * 255).astype('uint8')
         with self.writer.as_default():
-            tf.summary.image(name="Original", data=image, step=0)
+            tf.summary.image(name="Original", data=self.sample_image, step=0)
 
     def on_epoch_end(self, epoch, logs=None):
         super(TensorBoardImage, self).on_epoch_end(epoch, logs)
@@ -27,9 +26,7 @@ class TensorBoardImage(keras.callbacks.Callback):
             self.last_save = 0
 
             prediction = self.model.predict(self.sample_image)
-            prediction = np.clip(prediction, 0, 1) * 255
-            prediction = prediction.astype('uint8')
-            prediction = np.reshape(prediction, (-1, 128, 128, 1))
+            prediction = np.reshape(prediction, (-1, 128, 128, 4))
 
             with self.writer.as_default():
                 tf.summary.image(name=self.tag, data=prediction, step=epoch, max_outputs=len(self.sample_image))
